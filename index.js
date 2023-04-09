fc316d72863fa02e77999813b4117c5adf497b41
baovip0201 <baovip0201@gmail.com>
2023-04-09 18:36:55 +0700

    ...

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 const videoRouter=require("./routes/videos")
 const userRouter=require("./routes/user")
-const {connectDb}=require("../config/database")
+const {connectDb}=require("./config/database")
 require('dotenv').config()
 
 connectDb()
 
-app.use('/api/videos', videoRouter)
-app.use('/api', userRouter)
+app.use('/videos', videoRouter)
+app.use('/', userRouter)
 // Khởi động server
 app.listen(3000, () => {
   console.log('Server started on port 3000')
