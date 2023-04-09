bedc961f95d20aedab1a94e0b9be71b9770a4a75
baovip0201 <baovip0201@gmail.com>
2023-04-09 18:24:20 +0700

    change folder

--- api/index.js
+++ api/index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 const videoRouter=require("./routes/videos")
 const userRouter=require("./routes/user")
-const {connectDb}=require("./database/config")
+const {connectDb}=require("../config/database")
 require('dotenv').config()
 
 connectDb()
 
-app.use('/videos', videoRouter)
-app.use('/', userRouter)
+app.use('/api/videos', videoRouter)
+app.use('/api', userRouter)
 // Khởi động server
 app.listen(3000, () => {
   console.log('Server started on port 3000')
